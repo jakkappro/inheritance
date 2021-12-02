3af63530e45e7788ac21ffce64326f63ed8a6fdb
jakkappro <jakkappro@gmail.com>
2021-12-02 08:15:52 +0100

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,18 @@
-public class Main {
-    public static void main(String args[]) {
-        Programmer programmer = new Programmer(100, 1);
-        Teacher teacher = new Teacher(50, 1);
+import Model.*;
 
-        System.out.println(programmer.getInfo());
-        System.out.println(teacher.getInfo());
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Employee> employees = new ArrayList<>(List.of(new Programmer(100, 1),
+                new Teacher(50, 1), new Driver(300, 2)));
+
+        employees.forEach(n -> System.out.println(n.getInfo()));
+
+        EmployeeService service = new EmployeeServiceImpl();
+
+        System.out.println(service.totalSumOfBonuses(employees));
+        System.out.println(service.totalSumOfSalaries(employees));
     }
 }
